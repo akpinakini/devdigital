7e1c9d6ddd3da8b5222f4071e18357829a9a4638
abhi069 <abhishek.behera@iserveu.in>
2023-06-28 16:30:38 +0530

    issues solved

---
+++ app/src/main/java/com/assignment/mapdevDigital/BaseApplication.kt
@@ -0,0 +1,12 @@
+package com.example.send_message_to_number
+
+import android.app.Application
+import android.telephony.SmsManager
+
+class BaseApplication:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
